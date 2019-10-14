dd24544dd02ca99c1dffe2d1179c64b27370914a
scaillava <santiagocaillava@gmail.com>
2019-10-14 00:07:25 -0300

    runnig service

--- src/main/java/com/scaillava/todolist/controller/TodoController.java
+++ src/main/java/com/scaillava/todolist/controller/TodoController.java
@@ -1,6 +1,7 @@
 package com.scaillava.todolist.controller;
 
 import com.scaillava.todolist.domain.TodoPOJO;
+import com.scaillava.todolist.model.Todo;
 import com.scaillava.todolist.service.TodoService;
 import com.weddini.throttling.Throttling;
 import com.weddini.throttling.ThrottlingException;
@@ -23,9 +24,9 @@ public class TodoController {
     //After 100 Request its gonna response 400 bad request
     @Throttling(limit = 100, timeUnit = TimeUnit.SECONDS, type = ThrottlingType.RemoteAddr)
     @RequestMapping(value = "/todo", method = RequestMethod.POST)
-    public ResponseEntity addTodo(@RequestBody TodoPOJO todo) {
+    public ResponseEntity saveTodo(@RequestBody Todo todo) {
         try {
-            todosService.addTodo(todo);
+            todosService.saveTodo(todo);
 
             return ResponseEntity
                     .status(HttpStatus.OK).body(null);
@@ -48,9 +49,9 @@ public class TodoController {
     //After 100 Request its gonna response 400 bad request
     @Throttling(limit = 100, timeUnit = TimeUnit.SECONDS, type = ThrottlingType.RemoteAddr)
     @RequestMapping(value = "/todo", method = RequestMethod.GET)
-    public ResponseEntity getTodos(@RequestParam String id, @RequestParam String description, @RequestParam String done) {
+    public ResponseEntity getTodos(@RequestParam(required = false) String id, @RequestParam(required = false) String description, @RequestParam(required = false) String done) {
         try {
-            java.util.List<TodoPOJO> todos = todosService.getTodos(id, description, done);
+            Iterable<Todo> todos = todosService.getTodos(id, description, done);
 
             return ResponseEntity
                     .status(HttpStatus.OK)
@@ -72,34 +73,10 @@ public class TodoController {
     //After 100 Request its gonna response 400 bad request
     @Throttling(limit = 100, timeUnit = TimeUnit.SECONDS, type = ThrottlingType.RemoteAddr)
     @RequestMapping(value = "/todo", method = RequestMethod.PUT)
-    public ResponseEntity updateTodo(@RequestBody TodoPOJO todo) {
+    public ResponseEntity updateTodo(@RequestBody Todo todo) {
         try {
-            todosService.updateTodo(todo);
+            todosService.saveTodo(todo);
 
-            return ResponseEntity
-                    .status(HttpStatus.OK)
-                    .body(null);
-
-        } catch (ThrottlingException ex) {
-            log.error(ex.getMessage());
-            return ResponseEntity
-                    .status(HttpStatus.TOO_MANY_REQUESTS)
-                    .body(ex.getMessage());
-        } catch (Exception ex) {
-            log.error(ex.getMessage());
-            return ResponseEntity
-                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(ex.getMessage());
-        }
-    }
-
-
-    //After 100 Request its gonna response 400 bad request
-    @Throttling(limit = 100, timeUnit = TimeUnit.SECONDS, type = ThrottlingType.RemoteAddr)
-    @RequestMapping(value = "/todo", method = RequestMethod.DELETE)
-    public ResponseEntity deleteTodo(@RequestParam String id) {
-        try {
-            todosService.deleteTodo(id);
             return ResponseEntity
                     .status(HttpStatus.OK)
                     .body(null);
--- src/test/java/com/scaillava/todolist/model/PersonTest.java
+++ src/test/java/com/scaillava/todolist/model/PersonTest.java
@@ -36,383 +36,54 @@ public class PersonTest {
         this.mockMvc = MockMvcBuilders.webAppContextSetup(this.appContext).build();
     }
 
-    @Test
-    public void correctCaseExample() {
+//    @Test
+//    public void correctCaseExample() {
+//
+//        try {
+//            String[] dna = {
+//                    "ATGCGA",
+//                    "CAGTGC",
+//                    "TTATGT",
+//                    "AGAAGG",
+//                    "CCCCTA",
+//                    "TCACTG"};
+//            MvcResult result = mockMvc.perform(
+//                    post("/mutant/")
+//                            .contentType(MediaType.APPLICATION_JSON_UTF8)
+//                            .content(getDNAPOJO(dna).getBytes()))
+//                    .andExpect(status().isOk()).andReturn();
+//
+//            String content = result.getResponse().getContentAsString();
+//            assertEquals(Boolean.toString(true), content);
+//        } catch (Exception ex) {
+//            log.error(ex.getMessage());
+//        }
+//    }
+//
+//    //Correct case with 2 verticals lines  AAAA & CCCC
+//    @Test
+//    public void correctCase2VerticalLines() {
+//
+//        try {
+//            String[] dna = {
+//                    "ATGCGT",
+//                    "ACGTAC",
+//                    "ATCTGC",
+//                    "AGAAGC",
+//                    "CTCCTC",
+//                    "TCACTG"};
+//            MvcResult result = mockMvc.perform(
+//                    post("/mutant/")
+//                            .contentType(MediaType.APPLICATION_JSON_UTF8)
+//                            .content(getDNAPOJO(dna).getBytes()))
+//                    .andExpect(status().isOk()).andReturn();
+//
+//            String content = result.getResponse().getContentAsString();
+//            assertEquals(Boolean.toString(true), content);
+//        } catch (Exception ex) {
+//            log.error(ex.getMessage());
+//        }
+//    }
 
-        try {
-            String[] dna = {
-                    "ATGCGA",
-                    "CAGTGC",
-                    "TTATGT",
-                    "AGAAGG",
-                    "CCCCTA",
-                    "TCACTG"};
-            MvcResult result = mockMvc.perform(
-                    post("/mutant/")
-                            .contentType(MediaType.APPLICATION_JSON_UTF8)
-                            .content(getDNAPOJO(dna).getBytes()))
-                    .andExpect(status().isOk()).andReturn();
-
-            String content = result.getResponse().getContentAsString();
-            assertEquals(Boolean.toString(true), content);
-        } catch (Exception ex) {
-            log.error(ex.getMessage());
-        }
-    }
-
-    //Correct case with 2 verticals lines  AAAA & CCCC
-    @Test
-    public void correctCase2VerticalLines() {
-
-        try {
-            String[] dna = {
-                    "ATGCGT",
-                    "ACGTAC",
-                    "ATCTGC",
-                    "AGAAGC",
-                    "CTCCTC",
-                    "TCACTG"};
-            MvcResult result = mockMvc.perform(
-                    post("/mutant/")
-                            .contentType(MediaType.APPLICATION_JSON_UTF8)
-                            .content(getDNAPOJO(dna).getBytes()))
-                    .andExpect(status().isOk()).andReturn();
-
-            String content = result.getResponse().getContentAsString();
-            assertEquals(Boolean.toString(true), content);
-        } catch (Exception ex) {
-            log.error(ex.getMessage());
-        }
-    }
-
-    //Correct case with 2 horizontal lines TTTT & CCCC
-    @Test
-    public void correctCase2HorizontalLines() {
-
-        try {
-            String[] dna = {
-                    "ATGCGA",
-                    "CAGTGC",
-                    "TGTTTT",
-                    "AGAAGG",
-                    "CCCCTA",
-                    "TCACTG"};
-            MvcResult result = mockMvc.perform(
-                    post("/mutant/")
-                            .contentType(MediaType.APPLICATION_JSON_UTF8)
-                            .content(getDNAPOJO(dna).getBytes()))
-                    .andExpect(status().isOk()).andReturn();
-
-            String content = result.getResponse().getContentAsString();
-            assertEquals(Boolean.toString(true), content);
-        } catch (Exception ex) {
-            log.error(ex.getMessage());
-        }
-    }
-
-    //Correct case with 2 indiagonals lines LR AAAA & CCCC
-    @Test
-    public void correctCase2IndiagonalLR() {
-
-        try {
-            String[] dna = {
-                    "ATGCGA",
-                    "CAGTGC",
-                    "TCATCT",
-                    "AGCAGG",
-                    "CTCCTA",
-                    "TCACTG"};
-
-            MvcResult result = mockMvc.perform(
-                    post("/mutant/")
-                            .contentType(MediaType.APPLICATION_JSON_UTF8)
-                            .content(getDNAPOJO(dna).getBytes()))
-                    .andExpect(status().isOk()).andReturn();
-
-            String content = result.getResponse().getContentAsString();
-            assertEquals(Boolean.toString(true), content);
-        } catch (Exception ex) {
-            log.error(ex.getMessage());
-        }
-    }
-
-    //Correct case with 2 indiagonals lines RL TTTT & GGGG
-    @Test
-    public void correctCase2IndiagonalRL() {
-
-        try {
-            String[] dna = {
-                    "ATGCGA",
-                    "CAGGAC",
-                    "TTGTGT",
-                    "AGAATG",
-                    "CACTTA",
-                    "TCTCTG"};
-            MvcResult result = mockMvc.perform(
-                    post("/mutant/")
-                            .contentType(MediaType.APPLICATION_JSON_UTF8)
-                            .content(getDNAPOJO(dna).getBytes()))
-                    .andExpect(status().isOk()).andReturn();
-
-            String content = result.getResponse().getContentAsString();
-            assertEquals(Boolean.toString(true), content);
-        } catch (Exception ex) {
-            log.error(ex.getMessage());
-        }
-    }
-
-
-    //Incorrect case with 1 verticals lines  AAAA
-    @Test
-    public void incorrectCase1VerticalLines() {
-
-        try {
-            String[] dna = {
-                    "ATGCGT",
-                    "ACGTAC",
-                    "ATCTGA",
-                    "AGAAGC",
-                    "CTCCTC",
-                    "TCACTG"};
-            MvcResult result = mockMvc.perform(
-                    post("/mutant/")
-                            .contentType(MediaType.APPLICATION_JSON_UTF8)
-                            .content(getDNAPOJO(dna).getBytes()))
-                    .andExpect(status().isForbidden()).andReturn();
-
-            String content = result.getResponse().getContentAsString();
-            assertEquals(Boolean.toString(false), content);
-        } catch (Exception ex) {
-            log.error(ex.getMessage());
-        }
-    }
-
-    //Incorrect case with 1 horizontal lines TTTT
-    @Test
-    public void incorrectCase1HorizontalLines() {
-
-        try {
-            String[] dna = {
-                    "ATGCGA",
-                    "CAGTGC",
-                    "TGTTTT",
-                    "AGAAGG",
-                    "CGCCTA",
-                    "TCACTG"};
-            MvcResult result = mockMvc.perform(
-                    post("/mutant/")
-                            .contentType(MediaType.APPLICATION_JSON_UTF8)
-                            .content(getDNAPOJO(dna).getBytes()))
-                    .andExpect(status().isForbidden()).andReturn();
-
-            String content = result.getResponse().getContentAsString();
-            assertEquals(Boolean.toString(false), content);
-        } catch (Exception ex) {
-            log.error(ex.getMessage());
-        }
-    }
-
-    //Incorrect case with 1 indiagonals lines LR AAAA
-    @Test
-    public void incorrectCase1IndiagonalLR() {
-
-        try {
-            String[] dna = {
-                    "ATGCGA",
-                    "CAGTGC",
-                    "TCATCT",
-                    "AGGAGG",
-                    "CTCCTA",
-                    "TCACTG"};
-            MvcResult result = mockMvc.perform(
-                    post("/mutant/")
-                            .contentType(MediaType.APPLICATION_JSON_UTF8)
-                            .content(getDNAPOJO(dna).getBytes()))
-                    .andExpect(status().isForbidden()).andReturn();
-
-            String content = result.getResponse().getContentAsString();
-            assertEquals(Boolean.toString(false), content);
-        } catch (Exception ex) {
-            log.error(ex.getMessage());
-        }
-    }
-
-    //Incorrect case with 1 indiagonals lines RL TTTT
-    @Test
-    public void incorrectCase1IndiagonalRL() {
-
-        try {
-            String[] dna = {
-                    "ATCCGA",
-                    "CATCAC",
-                    "TTGTGT",
-                    "AGAATG",
-                    "CACGTA",
-                    "TCTCTG"};
-            MvcResult result = mockMvc.perform(
-                    post("/mutant/")
-                            .contentType(MediaType.APPLICATION_JSON_UTF8)
-                            .content(getDNAPOJO(dna).getBytes()))
-                    .andExpect(status().isForbidden()).andReturn();
-
-            String content = result.getResponse().getContentAsString();
-            assertEquals(Boolean.toString(false), content);
-        } catch (Exception ex) {
-            log.error(ex.getMessage());
-        }
-    }
-
-
-    //Incorrect case without lines
-    @Test
-    public void incorrectCaseAnyLine() {
-
-        try {
-            String[] dna = {
-                    "ATGCGA",
-                    "CAGTAC",
-                    "TTGTGT",
-                    "AGAATG",
-                    "CACGTA",
-                    "TCTCTG"};
-            MvcResult result = mockMvc.perform(
-                    post("/mutant/")
-                            .contentType(MediaType.APPLICATION_JSON_UTF8)
-                            .content(getDNAPOJO(dna).getBytes()))
-                    .andExpect(status().isForbidden()).andReturn();
-
-            String content = result.getResponse().getContentAsString();
-            assertEquals(Boolean.toString(false), content);
-        } catch (Exception ex) {
-            log.error(ex.getMessage());
-        }
-    }
-
-    //Empty dna
-    @Test
-    public void incorrectCaseRequiredDNAException() {
-        try {
-            String[] dna = {
-            };
-            MvcResult result = mockMvc.perform(
-                    post("/mutant/")
-                            .contentType(MediaType.APPLICATION_JSON_UTF8)
-                            .content(getDNAPOJO(dna).getBytes()))
-                    .andExpect(status().isBadRequest()).andReturn();
-
-            String content = result.getResponse().getContentAsString();
-            assertEquals("DNA is requiered.", content);
-        } catch (Exception ex) {
-            log.error(ex.getMessage());
-        }
-    }
-
-    //Empty dna
-    @Test
-    public void incorrectCaseInvalidChar() {
-        try {
-            String[] dna = {
-                    "ZTGCGA",
-                    "CAGTAC",
-                    "TTGTGT",
-                    "AGAATG",
-                    "CACGTA",
-                    "TCTCTG"};
-            MvcResult result = mockMvc.perform(
-                    post("/mutant/")
-                            .contentType(MediaType.APPLICATION_JSON_UTF8)
-                            .content(getDNAPOJO(dna).getBytes()))
-                    .andExpect(status().isBadRequest()).andReturn();
-
-            String content = result.getResponse().getContentAsString();
-            assertEquals("The row has invalids characters, only characters available are [atcg]+.", content);
-        } catch (Exception ex) {
-            log.error(ex.getMessage());
-        }
-    }
-
-    //Null dna
-    @Test
-    public void incorrectCaseRequiredDNAException2() {
-        try {
-
-            MvcResult result = mockMvc.perform(
-                    post("/mutant/")
-                            .contentType(MediaType.APPLICATION_JSON_UTF8)
-                            .content("".getBytes()))
-                    .andExpect(status().isBadRequest()).andReturn();
-
-        } catch (Exception ex) {
-            log.error(ex.getMessage());
-        }
-    }
-
-    //Incorrect structure
-    @Test
-    public void incorrectCaseStructure() {
-
-        try {
-            String[] dna = {
-                    "ATGCG",
-                    "CAGTAC",
-                    "TTGTGT",
-                    "AGAATG",
-                    "CACGTA",
-                    "TCTCTG"};
-            MvcResult result = mockMvc.perform(
-                    post("/mutant/")
-                            .contentType(MediaType.APPLICATION_JSON_UTF8)
-                            .content(getDNAPOJO(dna).getBytes()))
-                    .andExpect(status().isBadRequest()).andReturn();
-
-            String content = result.getResponse().getContentAsString();
-            assertEquals("The structure of the dna is not correct. Must be 6x6.", content);
-        } catch (Exception ex) {
-            log.error(ex.getMessage());
-        }
-    }
-
-
-    @Test
-    public void statsTest() {
-
-        try {
-            correctCaseExample();
-            incorrectCase1HorizontalLines();
-            incorrectCase1IndiagonalLR();
-            String[] dna = {
-                    "ATGCG",
-                    "CAGTAC",
-                    "TTGTGT",
-                    "AGAATG",
-                    "CACGTA",
-                    "TCTCTG"};
-            MvcResult result = mockMvc.perform(
-                    get("/stats/")
-                            .contentType(MediaType.APPLICATION_JSON_UTF8)
-                            .content(getDNAPOJO(dna).getBytes()))
-                    .andExpect(status().isOk()).andReturn();
-
-            String content = result.getResponse().getContentAsString();
-            assertEquals("{\"count_mutant_dna\":1,\"count_human_dna\":2,\"ratio\":0.5}", content);
-        } catch (Exception ex) {
-            log.error(ex.getMessage());
-        }
-    }
-
-
-    private String getDNAPOJO(String[] dna) {
-        String dnaResult = "{\"dna\": [";
-
-        for (String dnaItem : dna) {
-            dnaResult += "\"" + dnaItem + "\"" + ",";
-        }
-        //remove last ","
-        if (dna.length > 0)
-            dnaResult = dnaResult.substring(0, dnaResult.length() - 1);
-
-        dnaResult += "]}";
-        return dnaResult;
-    }
-}
+ }
 
